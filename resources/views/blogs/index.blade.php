5f673581b0c75a8a99daaabe6c68312f7b8cc2a9
bryanlollers <bryanasprer@gmail.com>
2018-10-25 02:02:46 +0800

    10/25/2018

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -14,6 +14,27 @@
                             <div class="box-article">
                                 <div class="row">
                                     <div class="col-xs-12 col-sm-4">
+                                        <a href="{{ url('https://mikemccluskey.net/2018/10/15/writing-as-a-form-of-therapy/') }}" target="_blank">
+                                            <div style="position:relative;width:100%;height:164px;background:url({{{ url('public/storage/writing-as-a-form-of-therapy.jpg') }}}) center center no-repeat;background-size: cover;overflow:hidden">
+                                                <div class="layer" style="background-color: rgba(0, 0, 0, 0.0);position: absolute;top: 0;left: 0;width: 100%;height: 100%;padding:10px 15px">
+                                                    
+                                                    
+                                                </div>
+                                            </div>
+                                        </a>
+                                        
+                                    </div>
+                                    <div class="col-xs-12 col-sm-8">
+                                        <h1 class="title"><a href="{{ url('https://mikemccluskey.net/2018/10/15/writing-as-a-form-of-therapy/') }}" target="_blank">Writing as a Form of Therapy</a></h1>
+                                        <h4 class="text-white">People with psychological problems no longer have to worry going to clinics for therapy when they can perform their treatment anywhere at any given time.</h4>
+                                        <p><img class="article-thumbnail img-circle" src="{{{ url('public/storage/featured-author/mmccluskey.jpg') }}}" style="width:20px;height:20px;margin-right:5px;margin-top:5px"> <a class="article-links" href="{{ url('https://mikemccluskey.net/') }}" target="_blank">Mike McCluskey</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="box-article">
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-4">
                                         <a href="{{ url('http://weightlossbible.net/2018/10/12/the-top-weight-loss-scams-in-the-world/') }}" target="_blank">
                                             <div style="position:relative;width:100%;height:164px;background:url({{{ url('public/storage/weightlossbibleblog.jpg') }}}) center center no-repeat;background-size: cover;overflow:hidden">
                                                 <div class="layer" style="background-color: rgba(0, 0, 0, 0.0);position: absolute;top: 0;left: 0;width: 100%;height: 100%;padding:10px 15px">
@@ -31,6 +52,7 @@
                                     </div>
                                 </div>
                             </div>
+                            <hr>
                             <div class="box-article">
                                 <div class="row">
                                     <div class="col-xs-12 col-sm-4">
@@ -57,6 +79,7 @@
                             <p><strong>RECENT POSTS</strong></p>
                             
                             <ul class="list-unstyled">
+                                <li><a href="{{ url('https://mikemccluskey.net/2018/10/15/writing-as-a-form-of-therapy/') }}" class="article-link" target="_blank">Writing as a Form of Therapy</a></li>
                                 <li><a href="{{ url('http://weightlossbible.net/2018/10/12/the-top-weight-loss-scams-in-the-world/') }}" class="article-link" target="_blank">The Top Weight Loss Scams in the World</a></li>
                                 <li><a href="{{ url('https://stevenbentleybooks.com/2018/09/03/top-5-most-accurate-medical-dramas/') }}" class="article-link" target="_blank">Top 5 Most Accurate Medical Dramas</a></li>
                             </ul>
