34d37115d3b1d67d5c9288c4dbe8da5037c94731
bryanlollers <bryanasprer@gmail.com>
2018-10-27 02:37:21 +0800

    10/27

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\HttpsProtocol::class,
-        \Watson\Canonical\CanonicalMiddleware::class,
+        // \Watson\Canonical\CanonicalMiddleware::class,
     ];
 
     /**
