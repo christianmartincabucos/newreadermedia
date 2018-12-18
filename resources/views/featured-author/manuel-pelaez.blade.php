880a7d0e031525f374e820a5fb0729726130da0b
bryanlollers <bryanasprer@gmail.com>
2018-12-19 01:13:52 +0800

    12/19

--- resources/views/featured-author/manuel-pelaez.blade.php
+++ resources/views/featured-author/manuel-pelaez.blade.php
@@ -26,23 +26,45 @@
                             <h4 class="text-center"><small>follow Manuel Pelaez on</small><br><a href="https://instagram.com/mannypel" target="_blank" rel="nofollow"><span class="fa fa-instagram"></span> Instagram</a> <small>|</small> <a href="https://twitter.com/ManuelP96782044" target="_blank" rel="nofollow"><span class="fa fa-twitter"></span> Twitter</a></h4>
                             <br>
                             
-                            
+                            <div class="divider"></div>
                             <div class="row">
                                 <div class="col-md-8 center-block">
                                     <a href="https://www.amazon.com/Project-Manuel-Pelaez/dp/1543475426" target="_blank" rel="nofollow">
-                                        <img src="{{{ url('public/storage/featured-author/the-project.png') }}}" class="" style="width:100%" alt="Messages from God: Hear God Like Never Before">
+                                        <img src="{{{ url('public/storage/featured-author/the-project.png') }}}" class="" style="width:100%" alt="The Project">
                                     </a>
                                 </div>
                                 
                             </div>
                             <h3 class="text-center"><strong>The Project</strong> </h3>
                             <p>Two teenagers different lives, caught up in over their heads in secrets, a mirage of events, assassin squads in every turn, government investigations, many battles between high tech machines against terrorist and rebel groups. One decides to hide among the ancient warlords and their underground forbidden cities, the other meets his fate, the secrets lead to suicide, and a secret team is assembled for a worldwide hunt.</p>
-                            <br>
-                            
-                            
+                            <h4 class="text-center"><small>Get a copy of the book on</small> <a href="https://www.amazon.com/Project-Manuel-Pelaez/dp/1543475426" target="_blank" rel="nofollow">amazon</a></h4>
+                            <div class="divider"></div>
+                            <div class="row">
+                                <div class="col-md-8 center-block">
+                                    <a href="https://www.amazon.com/Hunt-Manuel-Pelaez/dp/1543482562/" target="_blank" rel="nofollow">
+                                        <img src="{{{ url('public/storage/featured-author/the-hunt.png') }}}" class="" style="width:100%" alt="The Hunt">
+                                    </a>
+                                </div>
+                                
+                            </div>
+                            <h3 class="text-center"><strong>The Hunt</strong> </h3>
+                            <p>Agent Jones from the FBI assembles a special team under strict orders from a United States congressman to hunt down four rouge CIA operatives hiding in different remote country regions. Using informants and intel from other government secret services, they are planning covert operations to minimize casualties. The end result brings Marcus out of hiding in Africa to start a new beginning to a top secret robotic program that will save human soldier lives for futures to come.</p>
+                            <h4 class="text-center"><small>Get a copy of the book on</small> <a href="https://www.amazon.com/Hunt-Manuel-Pelaez/dp/1543482562/" target="_blank" rel="nofollow">amazon</a></h4>
 
+                            <div class="divider"></div>
+                            <div class="row">
+                                <div class="col-md-8 center-block">
+                                    <a href="https://www.amazon.com/dp/1984516043" target="_blank" rel="nofollow">
+                                        <img src="{{{ url('public/storage/featured-author/the-program.png') }}}" class="" style="width:100%" alt="The Program">
+                                    </a>
+                                </div>
+                                
+                            </div>
+                            <h3 class="text-center"><strong>The Program</strong> </h3>
+                            <p>Agent Jones, from the FBI, and his special team, return back to the states, after fierce covert operations, bringing an end to the four rouge CIA operatives. Julie, helps bring Marcus, out of hiding in Africa, and bring celebrations, medals, promotions, and a new future, and a new team, new leadership, to the robotic engineering scientists program. The assignmentis in brutal Afghanistan, where the program will be tested, beyond any imagination. The team is joined by military strategists, and experts, using the latest technologies available, for aerial spider drones, robotic super soldiers, to make history, save lives, solve problems, for futures to come.</p>
                             
-                            <h4 class="text-center"><small>Get a copy of the book on</small><br><a href="https://www.amazon.com/Project-Manuel-Pelaez/dp/1543475426" target="_blank" rel="nofollow">Amazon</a></h4>
+                            
+                            <h4 class="text-center"><small>Get a copy of the book on</small> <a href="https://www.amazon.com/dp/1984516043" target="_blank" rel="nofollow">amazon</a></h4>
                             
                         </div>
                         @include('featured-author.sidebar')
