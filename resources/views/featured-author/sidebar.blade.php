151f3b6be359c7b4ea934e248b3b4b9b122af097
bryanlollers <bryanasprer@gmail.com>
2019-04-06 00:45:51 +0800

    4/6

--- resources/views/featured-author/sidebar.blade.php
+++ resources/views/featured-author/sidebar.blade.php
@@ -18,7 +18,6 @@
         <li><a href="{{ route('spotlight.lindy-kerr') }}" class="article-link">Lindy Kerr</a></li>
         <li><a href="{{ route('spotlight.manuel-pelaez') }}" class="article-link">Manuel Pelaez</a></li>
         <li><a href="{{ route('spotlight.wendy-sellers') }}" class="article-link">Wendy Sellers</a></li>
-        <li class="hidden"><a href="{{ route('spotlight.yvon-bell') }}" class="article-link">Yvon Bell</a></li>
         <li><a href="{{ route('spotlight.tom-peavler') }}" class="article-link">Tom Peavler</a></li>
         <li><a href="{{ route('spotlight.philip-persinger') }}" class="article-link">Philip B. Persinger</a></li>
         <li><a href="{{ route('spotlight.lawrence-stentzel') }}" class="article-link">Lawrence Stentzel III</a></li>
@@ -26,16 +25,18 @@
         <li><a href="{{ route('spotlight.calvin-moir') }}" class="article-link">Calvin Moir</a></li>
         <li><a href="{{ route('spotlight.burt-kempner') }}" class="article-link">Burt Kempner</a></li>
         <li><a href="{{ route('spotlight.abid-shakir') }}" class="article-link">Abid Shakir</a></li>
-        <li><a href="{{ route('spotlight.sharon-brown-keith') }}" class="article-link">Sharon Brown Keith</a></li>
-        <li><a href="{{ route('spotlight.skyelar-nelson') }}" class="article-link">Skyelar Nelson</a></li>
         <li><a href="{{ route('spotlight.mike-mccluskey') }}" class="article-link">Mike McCluskey</a></li>
         <li><a href="{{ route('spotlight.les-klinefelter') }}" class="article-link">Les Klinefelter</a></li>
-        <li><a href="{{ route('spotlight.cora-darrah') }}" class="article-link">Cora Darrah</a></li>
         <li><a href="{{ route('spotlight.charles-campise') }}" class="article-link">Charles Campise</a></li>
         <li><a href="{{ route('spotlight.kristy-kraft') }}" class="article-link">Kristy K. Kraft</a></li>
-        <li class="hidden"><a href="{{ route('spotlight.zachary-zeigler') }}" class="article-link">Zachary Zeigler, Ph.D.</a></li>
         <li><a href="{{ route('spotlight.steven-bentley') }}" class="article-link">Steven Bentley, M.D.</a></li>
-        
+        <?php
+            // <li class="hidden"><a href="{{ route('spotlight.yvon-bell') }}" class="article-link">Yvon Bell</a></li>
+            // <li class="hidden"><a href="{{ route('spotlight.sharon-brown-keith') }}" class="article-link">Sharon Brown Keith</a></li>
+            // <li class="hidden"><a href="{{ route('spotlight.skyelar-nelson') }}" class="article-link">Skyelar Nelson</a></li>
+            // <li class="hidden"><a href="{{ route('spotlight.cora-darrah') }}" class="article-link">Cora Darrah</a></li>
+            // <li class="hidden"><a href="{{ route('spotlight.zachary-zeigler') }}" class="article-link">Zachary Zeigler, Ph.D.</a></li>
+        ?>
     </ul>
     
 </div>
